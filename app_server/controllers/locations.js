0ad92fcbc53d6b751625b340bf3295695572ded7
ma-za-kpe <makpalyy@gmail.com>
2018-10-12 00:16:17 +0300

    feed data to the view from the controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET 'home' page */
 router.homelist = function(req, res){
-    res.render('index', { title: 'Home' });
+    res.render('index', 
+    { 
+    title: 'Loc8r - find a place to work with wifi' ,
+    pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+    locations:[{
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+        },
+        {
+        name: 'Starcups',
+        address: '125 High Street, Reading, RG6 1PS',
+        rating: 3,
+        facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+        distance: '100m'
+        },
+        {
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }]
+    });
 };
 
 /* GET 'Location info' page */
