d1ea7b216ffdffab090181419c79b869afdf322c
ma-za-kpe <makpalyy@gmail.com>
2018-10-11 22:38:39 +0300

    Add about page

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page */
 router.about = function(req, res){
-    res.render('index', { title: 'About' });
+    res.render('generic-text', { title: 'About' });
 };
 
 module.exports = router;
